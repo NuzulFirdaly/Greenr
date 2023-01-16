739dece553fd590212aea2f65960a2a887d68108
NuzulFirdaly <nuzul5567@gmail.com>
2023-01-16 17:39:49 +0800

    update db config

--- config/DBConfig.js
+++ config/DBConfig.js
@@ -1,22 +1,18 @@
+const fs = require('fs');
+
+
 const Sequelize = require('sequelize');
 //bring in db.js which contains database name, username and password
 const db = require('./db');
 
 // Instantiates Sequelize with database parameters
 const sequelize = new Sequelize(db.database, db.username, db.password, {
-    host: db.host, // Name or IP address of MySQL server
     dialect: 'mysql', // Tells squelize that MySQL is used
-    operatorsAliases: false,
+    host: db.host, // Name or IP address of MySQL server
+    port: '3306',
     define: {
         timestamps: false // Don't create timestamp fields in database
     },
-    pool: { // Database system params, don't need to know
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    },
-    timezone: "+08:00"
 });
 
 
--- config/db.js
+++ config/db.js
@@ -1,7 +1,11 @@
+const dotenv = require("dotenv")
+
+dotenv.config()
 module.exports = {
 
-    host: 'localhost',
-    database: 'greenr',
-    username: 'greenradmin',
-    password: 'greenradmin123'
+
+    host: process.env.db_host,
+    database: process.env.db_database,
+    username: process.env.db_username,
+    password: process.env.db_password
 }
